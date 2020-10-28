53b9c5faf6fb158a30a50990a85dcd42a2e74ba8
b-hash <brianmhayashi@gmail.com>
2020-10-28 16:55:29 -0700

    (update) profile search bar handler

--- client/Components/Header.tsx
+++ client/Components/Header.tsx
@@ -1,26 +1,35 @@
-/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-use-before-define */
 import React, { useState } from 'react';
 
 const Header = () => {
-
   const [repoSearch, setRepoSearch] = useState('');
 
   const handleEnter = (e: any) => {
     if (e.key === 'Enter') {
-      console.log('Search Repo: ' + repoSearch);
-      // setRepoSearch('');
+      console.log(`Search Repo: ${repoSearch}`);
+      setRepoSearch('');
     }
   };
 
   return (
     <div id="header-container">
       <div id="profile-pic-div">
-        <img id="profile-pic" src="https://i.imgur.com/rY1b33l.jpg" alt="GitHub profile img" />
+        <img
+          id="profile-pic"
+          src="https://i.imgur.com/rY1b33l.jpg"
+          alt="GitHub profile img"
+        />
       </div>
       <div id="header-right">
         <div id="header-links">
-          <a className="header-btn" id="my-repos-header">My Repositories</a>
-          <a className="header-btn" id="starred-header">Starred Repositories</a>
+          <a className="header-btn" id="my-repos-header">
+            My Repositories
+          </a>
+          <a className="header-btn" id="starred-header">
+            Starred Repositories
+          </a>
         </div>
         <div id="header-search">
           <input
@@ -29,6 +38,7 @@ const Header = () => {
             placeholder="Find Repository"
             onChange={(e) => setRepoSearch(e.target.value)}
             onKeyDown={handleEnter}
+            value={repoSearch}
           />
         </div>
       </div>
