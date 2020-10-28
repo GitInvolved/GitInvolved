e6648134c48c38a15c35de141c83144c87dbc090
jpinchak <jpinchak@gmail.com>
2020-10-28 10:10:04 -0700

    create React component, set up Webpack to configure React component, add build links to index.html
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: B-Hash <BrianMHayashi@gmail.com>

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const path = require('path');
+import path from 'path';
 
 app.use(express.static('public'));
-//app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use('/build', express.static(path.resolve(__dirname, '../../build')));
+
+// app.get('/', (_req: any, res) => {
+//     res.status(200).sendFile(path.resolve(__dirname, '../../public/index.html'));
+//   });
 
 app.listen(3000, () => console.log('Listening on 3000'));
