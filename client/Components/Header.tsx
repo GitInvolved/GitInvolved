be0c59a910ae4752dcfcc7080f4d4c4c5e5925c1
b-hash <brianmhayashi@gmail.com>
2020-10-28 14:59:14 -0700

    (update) Header component and styling
    Co-authored-by: Julie <jpinchak@gmail.com>

--- client/Components/Header.tsx
+++ client/Components/Header.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      <div id="header-container">
-        <img id="profile-pic" src="https://i.imgur.com/geQRMcr.png" title="source: imgur.com" />
-        <a><p className="header-btn" id="my-repos-header">My Repositories</p></a>
-        <a><p className="header-btn" id="starred-header">Starred</p></a>
+    <div id="header-container">
+      <div id="profile-pic-div">
+        <img id="profile-pic" src="https://i.imgur.com/rY1b33l.jpg" title="source: imgur.com" />
       </div>
-      <input type="text" placeholder="Find Repository"></input>
+      <div id="header-right">
+        <div id="header-links">
+          <a className="header-btn"><p id="my-repos-header">My Repositories</p></a>
+          <a className="header-btn"><p id="starred-header">Starred</p></a>
+        </div>
+        <div id="header-search">
+          <input id="" type="text" placeholder="Find Repository"></input>
+        </div>
+      </div>
     </div>
   )
 }
