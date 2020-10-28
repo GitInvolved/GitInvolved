eb8ce6a94cdc22a28d7e0c82991e20664a806fda
b-hash <brianmhayashi@gmail.com>
2020-10-27 17:12:14 -0700

    (update) adds out and build folders to gitignore

--- out/server/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static('public'));
-//app.use('/build', express.static(path.join(__dirname, '../build')));
-app.listen(3000, () => console.log('Listening on 3000'));
-//# sourceMappingURL=server.js.map
