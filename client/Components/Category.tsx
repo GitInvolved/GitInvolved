4eb4fb0ad8dbca433252edd77ed2ce54a10ab4e0
b-hash <brianmhayashi@gmail.com>
2020-10-28 12:21:32 -0700

    (add) category container and component
    Co-authored-by: Jesus <jmodestov@gmail.com>
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Julie <jpinchak@gmail.com>
    Co-authored-by: Sanjay <sanjaylavingia@gmail.com>

---
+++ client/Components/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+  return (
+    <div>
+      <a><p>Add Category +</p></a>
+    </div>
+  )
+}
+
+export default Category;
---
+++ client/Containers/CategoryContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Category from '../Components/Category';
+
+const CategoryContainer = () => {
+  return (
+    <div>
+      <h2>Category:</h2>
+      <Category />
+    </div>
+  )
+}
+
+export default CategoryContainer;
