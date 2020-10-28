3d389a08387b4288ae4bfdf52d1214b62eed5d48
b-hash <brianmhayashi@gmail.com>
2020-10-28 12:33:58 -0700

    (update) styles for repo and Profile page
    Co-authored-by: Jesus <jmodestov@gmail.com>
    Co-authored-by: Ronnie <ronniemail20@gmail.com>
    Co-authored-by: Julie <jpinchak@gmail.com>
    Co-authored-by: Sanjay <sanjaylavingia@gmail.com>

--- client/Containers/ProfileContainer.tsx
+++ client/Containers/ProfileContainer.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Header from '../Components/Header';
 import CategoryContainer from './CategoryContainer';
+import RepoContainer from './RepoContainer';
 
 const ProfileContainer = () => {
   return (
     <div>
       <Header />
-      <CategoryContainer />
+      <div id="cat-and-repo">
+        <CategoryContainer />
+        <RepoContainer />
+      </div>
     </div>
   )
 }
