a1a7b11604eb03efa749366f6d50f45f240a50b8
Ronniegit config --global user.email ronniemail20@gmail.com <ronniemail20@gmail.com>
2020-10-28 21:45:18 -0700

    (Update) Add auth endpoint and change server.ts to js for experiment
    Co-authored-by: Jesus <jmodestov@gmail.com>
    Co-authored-by: Julie <jpinchak@gmail.com>
    Co-authored-by: B-Hash <BrianMHayashi@gmail.com>
    Co-authored-by: Sanjay <sanjaylavingia@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import path from 'path';
+const path = require('path');
+// import express from 'express';
+// import path from 'path';
+// import authRouter from './routers/authRouter';
+
+const authRouter = require('./routers/authRouter');
 
 app.use(express.static('public'));
 app.use('/build', express.static(path.resolve(__dirname, '../../build')));
@@ -8,5 +13,7 @@ app.use('/build', express.static(path.resolve(__dirname, '../../build')));
 // app.get('/', (_req: any, res) => {
 //     res.status(200).sendFile(path.resolve(__dirname, '../../public/index.html'));
 //   });
+
+app.use('/auth', authRouter);
 
 app.listen(3000, () => console.log('Listening on 3000'));
