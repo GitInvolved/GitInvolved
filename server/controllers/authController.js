acbcfc161a1126db5bdb2a1ef23a5065e8cb1646
Ronniegit config --global user.email ronniemail20@gmail.com <ronniemail20@gmail.com>
2020-10-29 03:13:59 -0700

    (Add) Set up authController and add getData middleware
    Co-authored-by: Jesus <jmodestov@gmail.com>
    Co-authored-by: Julie <jpinchak@gmail.com>
    Co-authored-by: B-Hash <BrianMHayashi@gmail.com>
    Co-authored-by: Sanjay <sanjaylavingia@gmail.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,47 +1,32 @@
+const fetch = require('node-fetch');
 const authController = {};
 const clientId = '21b87ca77f7f56db3dcf';
-const clientSecret = '<YOUR_CLIENT_SECRET>';
-const redirectUrl = '<YOUR_REDIRECT_URL>';
-
-authController.getToken = (req, res, next) => {
-  //const githubId = [req.user.id];
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=21b87ca77f7f56db3dcf`);
-  console.log('getToken');
-  return next();
-};
+const clientSecret = '04c60e8e8bddaaaeb6066b76b18198ccef6608e0';
+const redirectUrl = 'http://localhost:3000/auth/profile';
 
 authController.getData = (req, res, next) => {
+  //Receive the code parameter from GitHub API after user signin there
   const requestToken = req.query.code;
-  const githubUrl = `https://github.com/login/oauth/${requestToken}`;
+  const githubUrl = `https://github.com/login/oauth/access_token?client_id=${clientId}&redirect_uri=${redirectUrl}&client_secret=${clientSecret}&code=${requestToken}`;
 
-  //if GitHub API returns the code parameter
-  if (requestToken) {
-    const requestData = {
-      clientId: clientId,
-      redirectUrl: redirectUrl,
-      clientSecret: clientSecret,
-      code: requestToken,
-    };
-
-    fetch(githubUrl, {
-      method: 'POST',
-      body: JSON.stringify(requestData),
-      headers: {
-        accept: 'application/json',
-      },
+  //Post request to another GitHub API endpoint for an access token
+  fetch(githubUrl, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const access_token = data.access_token;
+      res.redirect(`/profile.html?access_token=${access_token}`);
+      return next();
     })
-      .then((response) => response.json())
-      .then((data) => {
-        const accessToken = data.access_token;
-        res.redirect(`/profile.html?access_token=${accessToken}`);
-        return next();
-      })
-      .catch((error) => {
-        console.log('fetch error');
-        return next(error);
-      });
-  }
+    .catch((error) => {
+      console.log('fetch error');
+      return next(error);
+    });
 };
 
-// console.log(authController);
-export default authController;
+// export default authController;
+module.exports = authController;
